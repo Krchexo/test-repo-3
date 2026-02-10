c397c7b00eb7085109b69f9efa78c69ba3f8cce0
Song <songzhang015@gmail.com>
2026-02-09 18:57:04 -0800

    Working go

---
+++ working-go/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is running smoothly!"))
+	})
+
+	fmt.Println("Starting working server on :8081...")
+	http.ListenAndServe(":8081", r)
+}
